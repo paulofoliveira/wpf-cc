094fea66c887803c4f0d2339c3c8a73e43bb5ec2
paulofoliveira <paulo.fdo@hotmail.com>
2024-08-03 15:41:21 -0300

    Cria ReadOnlyProp

--- CustomControlLib/MyCustomControl.cs
+++ CustomControlLib/MyCustomControl.cs
@@ -9,5 +9,33 @@ namespace CustomControlLib
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(MyCustomControl), new FrameworkPropertyMetadata(typeof(MyCustomControl)));
         }
+
+        private static readonly DependencyPropertyKey HasBeenClickedPropertyKey =
+            DependencyProperty.RegisterReadOnly("HasBeenClicked",
+                typeof(bool),
+                typeof(MyCustomControl),
+                new PropertyMetadata(false));
+
+        private static readonly DependencyProperty HasBeenClickedProperty = HasBeenClickedPropertyKey.DependencyProperty;
+
+        public bool HasBeenClicked => (bool)GetValue(HasBeenClickedProperty);
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            // TODO: Demo purposes only, check for previous instances and remove handler first.
+
+            var button = GetTemplateChild("PART_Button") as Button;
+
+            if (button != null)
+            {
+                button.Click += Button_Click;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetValue(HasBeenClickedPropertyKey, true);
+        }
     }
 }
