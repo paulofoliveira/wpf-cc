8cb40c0eacef26d63aa7c84543a0349f56cc15ca
paulofoliveira <paulo.fdo@hotmail.com>
2024-08-03 12:42:53 -0300

    Adiciona projeto inicial para exercícios

---
+++ CustomControlLib/MyCustomControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CustomControlLib
+{
+    public class MyCustomControl : Control
+    {
+        static MyCustomControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyCustomControl), new FrameworkPropertyMetadata(typeof(MyCustomControl)));
+        }
+    }
+}
