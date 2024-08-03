138a5c2e01449fc5008d9f997a6093b877f4d3dc
paulofoliveira <paulo.fdo@hotmail.com>
2024-08-03 16:56:59 -0300

    Inclui attached property IncludeChildCount

--- CustomControlApp/MainWindow.xaml.cs
+++ CustomControlApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using CustomControlLib;
+using System.ComponentModel;
 using System.Windows;
 
 namespace CustomControlApp
@@ -11,6 +12,11 @@ namespace CustomControlApp
         public MainWindow()
         {
             InitializeComponent();
+
+            // Formas diferentes de setar um valor a uma Attached Property:
+
+            _stackPanel.SetValue(MyCustomControl.IncludeChildCountProperty, true);
+            //MyCustomControl.SetIncludeChildCount(_stackPanel, true);
         }
     }
 
--- CustomControlLib/MyCustomControl.cs
+++ CustomControlLib/MyCustomControl.cs
@@ -1,13 +1,45 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace CustomControlLib
 {
-    public class MyCustomControl : Control
+    public class MyCustomControl : ContentControl
     {
         static MyCustomControl()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(MyCustomControl), new FrameworkPropertyMetadata(typeof(MyCustomControl)));
+        }
+
+        public int ChildCount
+        {
+            get => (int)GetValue(ChildCountProperty);
+            set => SetValue(ChildCountProperty, value);
+        }
+
+        public static readonly DependencyProperty ChildCountProperty =
+            DependencyProperty.Register("ChildCount", typeof(int), typeof(MyCustomControl), new PropertyMetadata(0));
+
+        public static readonly DependencyProperty IncludeChildCountProperty =
+            DependencyProperty.RegisterAttached("IncludeChildCount", typeof(bool), typeof(MyCustomControl), new PropertyMetadata(false));
+        public static bool GetIncludeChildCount(DependencyObject obj) => (bool)obj.GetValue(IncludeChildCountProperty);
+        public static void SetIncludeChildCount(DependencyObject obj, bool value) => obj.SetValue(IncludeChildCountProperty, value);
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+            if (Content == null) return;
+
+            if (GetIncludeChildCount(Content as DependencyObject))
+                ChildCount++;
+
+            var panel = Content as Panel;
+            if (panel == null) return;
+
+            foreach (FrameworkElement child in panel.Children)
+                if (GetIncludeChildCount(child))
+                    ChildCount++;
         }
     }
 }
