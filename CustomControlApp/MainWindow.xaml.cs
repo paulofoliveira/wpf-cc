559b9cb230a22246a8454d3b015eba220843dd05
paulofoliveira <paulo.fdo@hotmail.com>
2024-08-04 21:06:11 -0300

    Documenta RoutedEvents para Estudo

--- CustomControlApp/MainWindow.xaml.cs
+++ CustomControlApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace CustomControlApp
 {
@@ -11,6 +12,25 @@ namespace CustomControlApp
         public MainWindow()
         {
             InitializeComponent();
+
+            // Configura um Routed Event manualmente a partir do click de um Botão.
+
+            AddHandler(Button.ClickEvent, new RoutedEventHandler(Window_Click));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            e.Handled = true; // Cancela automaticamente eventos posteriores de elementos da arvore pela estratégia de Struggling.
+        }
+
+        private void Window_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 
