37b667433c36ff874f6ccccc135df1951f070121
paulofoliveira <paulo.fdo@hotmail.com>
2024-09-14 22:55:54 -0300

    Cria o cenário

--- CustomControlLib/MyCustomControl.cs
+++ CustomControlLib/MyCustomControl.cs
@@ -5,9 +5,18 @@ namespace CustomControlLib
 {
     public class MyCustomControl : Control
     {
+        private Button _button;
+
         static MyCustomControl()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(MyCustomControl), new FrameworkPropertyMetadata(typeof(MyCustomControl)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _button = GetTemplateChild("PART_Button") as Button;
         }
     }
 }
