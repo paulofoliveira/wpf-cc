341891a94a3c30fdac63edaa304d95d71c8a3c6e
paulofoliveira <paulo.fdo@hotmail.com>
2024-09-15 22:20:42 -0300

    Aplica criação de Triggers

--- CustomControlLib/MyCustomControl.cs
+++ CustomControlLib/MyCustomControl.cs
@@ -9,5 +9,13 @@ namespace CustomControlLib
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(MyCustomControl), new FrameworkPropertyMetadata(typeof(MyCustomControl)));
         }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(MyCustomControl), new PropertyMetadata(string.Empty));
     }
 }
