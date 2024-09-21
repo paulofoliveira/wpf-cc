19d39a2abbb211f15aa767a961d7ec54117b409e
paulofoliveira <paulo.fdo@hotmail.com>
2024-09-21 17:10:11 -0300

    Define tema Aero2

--- CustomControlLib/Properties/AssemblyInfo.cs
+++ CustomControlLib/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -30,14 +28,15 @@ using System.Windows;
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
+// ResourceDictionaryLocation.SourceAssembly (significa que suporta temas e que deve procurar neste assembly.
 
-[assembly:ThemeInfo(
-    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-                             //(used if a resource is not found in the page,
-                             // or application resource dictionaries)
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.SourceAssembly, //where theme specific resource dictionaries are located
+                                               //(used if a resource is not found in the page,
+                                               // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                      //(used if a resource is not found in the page,
-                                      // app, or any theme specific resource dictionaries)
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
 )]
 
 
