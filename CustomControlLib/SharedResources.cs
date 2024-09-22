6479f4c590d3d42bc1a4ed341f037b8f636145c2
paulofoliveira <paulo.fdo@hotmail.com>
2024-09-21 21:55:01 -0300

    Simplifica ComponentResourceKey com SharedResources

---
+++ CustomControlLib/SharedResources.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace CustomControlLib
+{
+    public static class SharedResources
+    {
+        private static ComponentResourceKey _brushKey = new ComponentResourceKey(typeof(SharedResources), "CommonBrush");
+        public static ComponentResourceKey CommonBrushKey => _brushKey;
+    }
+}
