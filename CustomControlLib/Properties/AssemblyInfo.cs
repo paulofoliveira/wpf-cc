5f980183c685423914a3a1058032d4dbb7e61b7c
paulofoliveira <paulo.fdo@hotmail.com>
2024-09-21 21:33:21 -0300

    Implementa XmlnsPrefix e XmlnsDefinition

--- CustomControlLib/Properties/AssemblyInfo.cs
+++ CustomControlLib/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Markup;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -52,3 +53,6 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: XmlnsPrefix("http://schema.paulofernando.dev/wpf", "cc")]
+[assembly: XmlnsDefinition("http://schema.paulofernando.dev/wpf", "CustomControlLib")]
